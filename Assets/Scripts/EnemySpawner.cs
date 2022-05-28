33a6ecc4208a0337c284f19419a34d562ecf58d0
Nick-0s <ol-sereg1202@yandex.ru>
2022-05-28 19:42:53 +0300

    add

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Transform _spawnPoints;
+
+    private bool _isWorking;
+    private Transform[] _points;
+
+    private void Start()
+    {
+        _points = new Transform[_spawnPoints.childCount];
+
+        for (int i = 0; i < _spawnPoints.childCount; i++)
+            _points[i] = _spawnPoints.GetChild(i);
+
+        _isWorking = true;
+        Coroutine spawn = StartCoroutine(Spawn());
+    }
+
+    private void OnMouseDown()
+    {
+        _isWorking = false;
+    }
+
+    private IEnumerator Spawn()
+    {
+        var waitForTwoSeconds = new WaitForSeconds(2f);
+        int pointIndex = 0;
+
+        while(_isWorking)
+        {
+            GameObject newObject = Instantiate(_enemy, _points[pointIndex].position, Quaternion.identity);
+            pointIndex = (pointIndex + 1) % _spawnPoints.childCount;
+
+            yield return waitForTwoSeconds;
+        }
+    }
+}
